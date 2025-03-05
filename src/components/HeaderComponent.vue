50b946e62f814cf83d68994914edf8e61f7f14c1
luail <kynia0506@naver.com>
2025-03-05 15:13:16 +0900

    testing

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -10,7 +10,7 @@
                     </div>
                 </v-col>
                 <v-col class="text-center">
-                    <v-btn :to="{path:'/'}">java를 java줘</v-btn>
+                    <v-btn :to="{path:'/'}">java를 java주지마!</v-btn>
                 </v-col>
                 <v-col class="d-flex justify-end">
                     <v-btn v-if="isLogin" :to="{path:'/order/cart'}">장바구니 {{ getTotalQauntity }}</v-btn>
